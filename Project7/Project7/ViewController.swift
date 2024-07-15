60f53965e6adfb692cfb4c79f014a0e3926c46f0
leskerito <franck.kindia@outlook.fr>
2024-07-15 13:36:41 +0200

    Starting Project7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Franck Kindia on 15/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
