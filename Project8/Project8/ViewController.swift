d523eee7e07a1819b8306aa8f1bdb97441e6e562
leskerito <franck.kindia@outlook.fr>
2024-07-16 14:37:51 +0200

    Starting Project8

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Franck Kindia on 16/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
