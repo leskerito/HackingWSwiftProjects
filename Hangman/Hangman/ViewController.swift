e16a921048d43df2c3d9b650b2f835b2a103ce83
leskerito <franck.kindia@outlook.fr>
2024-07-17 15:48:27 +0200

    Starting Milestone 3: The Hangman Game

---
+++ Hangman/Hangman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Franck Kindia on 17/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
