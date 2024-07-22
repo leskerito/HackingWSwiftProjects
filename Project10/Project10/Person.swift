d0acc5b445577dc53c4075122c3dc0e82f2f5f21
leskerito <franck.kindia@outlook.fr>
2024-07-22 16:32:39 +0200

    Finished project 10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Franck Kindia on 22/07/2024.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Franck Kindia on 22/07/2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
