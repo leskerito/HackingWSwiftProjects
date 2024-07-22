a36a71d986fa770ccffcc79f0f53d9050f38517e
leskerito <franck.kindia@outlook.fr>
2024-07-22 13:33:34 +0200

    Starting Project10

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Franck Kindia on 22/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
